4ed1b18704f118b6054fe4b21d5722ea84da352d
britilda <clotildaekueape@gmail.com>
2021-06-19 18:08:12 +0000

    first commit

---
+++ app/src/main/java/edu/iai/glsib/ratrapagejava/RatrapageAtivity.java
@@ -0,0 +1,14 @@
+package edu.iai.glsib.ratrapagejava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RatrapageAtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ratrapage_ativity);
+    }
+}
